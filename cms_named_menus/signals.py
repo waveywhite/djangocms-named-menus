cf877cbd2eb4d9bbaa34e8c97001caf95fbf5336
jakob <jakob.oswald@gmx.net>
2016-05-21 08:08:36 +0200

    Properly invalidating cache on child changes

--- cms_named_menus/signals.py
+++ cms_named_menus/signals.py
@@ -16,12 +16,17 @@ from django.dispatch.dispatcher import receiver
 def clear_cache_named_menu_saved(sender, instance, **kwargs):
     cache.delete(instance.name)
     
+def _contains_page(menu, page_id):
+    for page in menu.pages:
+        if page['id'] == page_id:
+            return True
+        for child in page.get('children', []):
+            if child['id'] == page_id:
+                return True
+            
 @receiver(post_save, sender=Title, dispatch_uid='clear_cache_title_saved')
 def clear_cache_title_saved(sender, instance, **kwargs):
-    page_id = instance.page.id
     for menu in CMSNamedMenu.objects.all():
-        for page in menu.pages:
-            if page['id'] == page_id:
-                cache.delete(menu.name, instance.language)
-                break
+        if _contains_page(menu, instance.page.id):
+            cache.delete(menu.name, instance.language)
     
