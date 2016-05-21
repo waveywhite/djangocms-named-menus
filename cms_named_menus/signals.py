39bef5abdf05e944f471ea7be746771f822378bb
jakob <jakob.oswald@gmx.net>
2016-05-21 08:13:42 +0200

    Moved utility function to separate module

--- cms_named_menus/signals.py
+++ cms_named_menus/signals.py
@@ -11,22 +11,16 @@ from cms.models.titlemodels import Title
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
+from cms_named_menus.utils import contains_page
+
 
 @receiver(post_save, sender=CMSNamedMenu, dispatch_uid='clear_cache_named_menu_saved')
 def clear_cache_named_menu_saved(sender, instance, **kwargs):
     cache.delete(instance.name)
     
-def _contains_page(menu, page_id):
-    for page in menu.pages:
-        if page['id'] == page_id:
-            return True
-        for child in page.get('children', []):
-            if child['id'] == page_id:
-                return True
-            
 @receiver(post_save, sender=Title, dispatch_uid='clear_cache_title_saved')
 def clear_cache_title_saved(sender, instance, **kwargs):
     for menu in CMSNamedMenu.objects.all():
-        if _contains_page(menu, instance.page.id):
+        if contains_page(menu, instance.page.id):
             cache.delete(menu.name, instance.language)
     
