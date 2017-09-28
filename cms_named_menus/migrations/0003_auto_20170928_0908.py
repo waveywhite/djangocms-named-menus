aacc5061a76cab69742b41a0f66d353597c4e4f0
JensDiemer <git@jensdiemer.de>
2017-09-28 11:10:53 +0200

    add missing migrations

---
+++ cms_named_menus/migrations/0003_auto_20170928_0908.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import autoslug.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cms_named_menus', '0002_auto_20150227_1436'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='cmsnamedmenu',
+            name='slug',
+            field=autoslug.fields.AutoSlugField(always_update=True, populate_from=b'name', editable=False),
+        ),
+    ]
