2fecc75da9fc149c02c0d521177d84dc237bca76
carderm <michael.carder@michaelcarder.co.uk>
2019-02-01 17:45:43 +1100

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
 
 setup(
     name='django-cms-named-menus',
-    version='0.2.0',
+    version='0.2.1',
     description='Allows you to add named menus like Wordpress',
     long_description=read(os.path.join(os.path.dirname(__file__), 'README.rst')),
     author='Ryan Bagwell',
@@ -23,5 +23,20 @@ setup(
         'django-cms>=3.3',
         'jsonfield>=1.0.0',
         'django-autoslug>=1.7.2',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.11',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
