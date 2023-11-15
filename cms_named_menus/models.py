7ba9c8a92d53908fe281e49f9e275b33973b95fe
David White <david@netriver.co.uk>
2023-11-15 10:41:42 +0000

    Update models.py
    Fix reliance on deprecated ugettext_lazy, replace with gettext_lazy

--- cms_named_menus/models.py
+++ cms_named_menus/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.sites.models import Site
 from autoslug.fields import AutoSlugField
 from jsonfield import JSONField
